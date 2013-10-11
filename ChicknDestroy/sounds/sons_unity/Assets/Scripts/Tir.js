783426d1f300667985557264b3a7c1dd8fe87245
armi7 <armillas7@hotmail.com>
2013-10-11 21:09:14 +0200

    #40 #39 #36 Incorporats sons a unity
    Sons del menú, de l'escopeta i l'ambiental del nivell 1.
    A més, feta animació de la bala un cop surt disparada de l'escopeta.
    Llegir README per a més informació.

---
+++ ChicknDestroy/sounds/sons_unity/Assets/Scripts/Tir.js
@@ -0,0 +1,37 @@
+﻿
+public var sortidaBala:GameObject;
+public var escopeta:GameObject;
+public var bala:GameObject;
+ 
+var audioTir: AudioSource;
+var audioRecarga: AudioSource;
+var velocitatBala = 1;
+ 
+function Start () {
+	//obtenim tots els audioSources
+	var aSources = GetComponents(AudioSource);
+	//assignem cada so
+	audioTir = aSources[0];
+    audioRecarga = aSources[1];
+    //assignem els objectes involucrats
+	escopeta = GameObject.Find("escopeta");
+	sortidaBala = GameObject.Find("sortidaBala");
+	bala = GameObject.Find("bala");
+}
+
+function Update () {
+	//Per disparar apretar la tecla T
+	if (Input.GetKeyDown(KeyCode.T)) {
+		var nouTir:GameObject=Instantiate (bala, sortidaBala.transform.position, escopeta.transform.rotation);
+		//per fer que la bala desapareixi al cap de X segons
+		nouTir.AddComponent("DestruirBala");
+		//segons l'eix on es col·loca la velocitat de la bala en Vector3, aquesta es desplaça cap a una direccio o una altre
+		nouTir.rigidbody.AddRelativeForce(new Vector3(0, 0, velocitatBala), ForceMode.VelocityChange);
+		audioTir.Play();
+	}
+	
+	if (Input.GetKeyDown(KeyCode.R)) {;
+		audioRecarga.Play();
+	}
+
+}
