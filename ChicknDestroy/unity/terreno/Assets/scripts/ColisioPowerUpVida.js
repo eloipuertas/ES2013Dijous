f32886bb47caf273d2de39bd71e4bc5cbe8510b1
armi7 <armillas7@hotmail.com>
2013-10-25 13:04:08 +0200

    #118 Efecte visual amb el power-up vida (testejar)

---
+++ ChicknDestroy/unity/terreno/Assets/scripts/ColisioPowerUpVida.js
@@ -0,0 +1,16 @@
+﻿var explosion: GameObject;
+public var powerUp7:GameObject;
+var audioPowerUp:AudioSource;
+
+function Start () {
+	powerUp7 = GameObject.Find("upVida_animacio_rotacio_7");
+	var aSources = GetComponents(AudioSource);
+	audioPowerUp = aSources[0];
+}
+ 
+ 
+function OnCollisionEnter(){
+     audioPowerUp.Play();
+     var expl = Instantiate(explosion, transform.position, Quaternion.identity);
+     Destroy(expl, 3); // delete the explosion after 3 seconds
+}
