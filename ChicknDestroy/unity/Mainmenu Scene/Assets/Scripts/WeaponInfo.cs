77cc81fa4c01e47caa03180562eaaa8313836673
qfornaguera <qfornaguera@gmail.com>
2013-10-26 20:18:39 +0200

    #90 Logic done but not ready to be tested
    This task is now functional but not ready to be tested due to the code
    that has not been commented yet and add the other weapon object
    possibilities

---
+++ ChicknDestroy/unity/Mainmenu Scene/Assets/Scripts/WeaponInfo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponInfo : MonoBehaviour {
+
+	public bool isEscopeta = false;
+	private bool isPlayer = false;
+	WWW w;
+	Texture2D t;
+	
+	void OnTriggerEnter(Collider whoIs) {
+        if(whoIs.CompareTag("Player")){
+			isPlayer = true;	
+		}
+    }
+	
+	void OnTriggerExit(Collider whoIs){
+		 if(whoIs.CompareTag("Player")){
+			isPlayer = false;	
+		}
+	}
+	
+	void OnGUI(){
+		if(isEscopeta && isPlayer){
+			print ("hola");
+			w = new WWW("file://"+Application.dataPath+"/Texture/hud/escopeta.png");
+			t = w.texture;
+			Vector2 pos2d = Camera.main.WorldToScreenPoint(this.transform.position);
+			//Vector2 pos2d = new Vector2(0,0);
+			Rect pos = new Rect(pos2d.x-50,pos2d.y+100,500,100);
+			GUI.Label(pos,t);
+		}
+	}
+}
