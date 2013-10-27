95a89bcbc660138c964f52256277fa7421827e00
José Rojas <jf.mail2007@gmail.com>
2013-10-28 00:00:38 +0100

    #58 #144 #69 #13 #12 #11 #10 #8 #9  V2

---
+++ ChicknDestroy/unity/sprint2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+	
+	// Player Handling
+	public float gravity = 40;
+	public float speed = 30;
+	public float acceleration = 30;
+	public float jumpHeight = 60;
+	
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToMove;
+	
+	
+	private int movDer = 1;
+	private int movIzq = 2;
+	private int stopDer = 3;
+	private int stopIzq = 4;
+
+	private int lastDirection;
+	
+	private PlayerPhysics playerPhysics;
+
+	
+	
+	void Start () {
+		playerPhysics = GetComponent<PlayerPhysics>();
+		animation.Play("paradaDerecha");
+		lastDirection = stopDer;
+	}
+	
+	void Update () {
+		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+		currentSpeed = IncrementTowards(currentSpeed, targetSpeed,acceleration);
+		
+		if (playerPhysics.grounded) {
+			amountToMove.y = 0;
+			
+			// Jump
+			if (Input.GetButtonDown("Jump")) {
+				amountToMove.y = jumpHeight;	
+			}
+		}
+		
+		
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move(amountToMove * Time.deltaTime);
+		
+		Debug.Log(amountToMove.x);
+		if(amountToMove.y > 1) {
+			if(lastDirection == movDer || lastDirection == stopDer) animation.Play("saltoVerticalDer");
+			else animation.Play("saltoVerticalIz");
+		}else if(amountToMove.y < -1){
+			if(lastDirection == movDer || lastDirection == stopDer) animation.Play("caidaDerecha");
+			else animation.Play("caidaIzquierda");
+		}else {
+			if (lastDirection == movDer) {
+				if (amountToMove.x > 0) animation.Play("correrDerecha");
+				else if(amountToMove.x < 0) animation.Play("giroDerechaIzq");
+				else {
+					animation.Play("paradaDerecha");
+					lastDirection = stopDer;
+				}
+			}else if (lastDirection == movIzq) {
+				if (amountToMove.x < 0) animation.Play("correrIzquierda");
+				else if(amountToMove.x > 0) animation.Play("giroIzquierdaDerecha");
+				else {
+					animation.Play("paradaIzquierda");
+					lastDirection = stopIzq;
+				}
+			}else {
+				if (lastDirection == stopDer) animation.Play("paradaDerecha");
+				else animation.Play("paradaIzquierda");
+			}
+		
+			if (amountToMove.x != 0) 
+				lastDirection = (amountToMove.x < 0)?movIzq:movDer;
+		}
+	}
+	
+	// Increase n towards target by speed
+	private float IncrementTowards(float n, float target, float a) {
+		if (n == target) {
+			return n;	
+		}
+		else {
+			float dir = Mathf.Sign(target - n); // must n be increased or decreased to get closer to target
+			n += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(target-n))? n: target; // if n has now passed target then return target, otherwise return n
+		}
+	}
+}
