80a012a24d88814e33c7259435c9e3acba6ebf51
armi7 <armillas7@hotmail.com>
2013-11-08 12:11:59 +0100

    #208 Afegit moviment ascensor

---
+++ ChicknDestroy/unity/Mainmenu Scene/Assets/Special_effects/Scripts/MovimentAscensor.js
@@ -0,0 +1,16 @@
+﻿
+var ascensor : GameObject;
+private var velocitat = 50;
+private var pujant = 0;
+
+function Update () {
+	if (ascensor.transform.position.y >= 460)
+		pujant = 0;
+	else if (ascensor.transform.position.y <= 218)
+		pujant = 1;
+
+	if (pujant == 1)
+		ascensor.transform.Translate(Vector3.up*Time.deltaTime*velocitat);
+	else 
+		ascensor.transform.Translate(Vector3.down*Time.deltaTime*velocitat);
+}
