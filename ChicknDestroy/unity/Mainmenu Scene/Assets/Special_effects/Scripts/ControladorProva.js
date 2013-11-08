d666dadc7c07a8f51fa8da77a83146f87c29ccc9
armi7 <armillas7@hotmail.com>
2013-11-08 14:02:27 +0100

    #209 Afegit halo a la bandera
    Es desactiva en interactuar amb algo que tingui collider.

---
+++ ChicknDestroy/unity/Mainmenu Scene/Assets/Special_effects/Scripts/ControladorProva.js
@@ -0,0 +1,16 @@
+﻿var Personatge:GameObject;
+
+function Update () {
+	if(Input.GetKeyUp(KeyCode.Space)) {
+		Personatge.rigidbody.AddForce(new Vector3(0,50,0), ForceMode.VelocityChange);
+	}
+
+	if(Input.GetKey("left")){
+		Personatge.transform.position.x -= 1;
+	}
+	
+	if(Input.GetKey("right")){
+		Personatge.transform.position.x += 1;
+	}
+
+}
---
+++ ChicknDestroy/unity/Mainmenu Scene/Assets/Special_effects/Scripts/HaloBandera.js
@@ -0,0 +1,34 @@
+﻿
+var bandera : GameObject;
+var audioPowerUp : AudioSource;
+
+private var halo;
+private var tempsCanvi = 1;
+private var ultimCanvi = 0;
+private var deshabilitat = 0;
+
+function Update () {
+	if (deshabilitat == 0) {
+		if ((Time.time - ultimCanvi) > tempsCanvi) {
+				
+			if (bandera.GetComponent("Halo").enabled)
+				bandera.GetComponent("Halo").enabled = false;
+			else 
+				bandera.GetComponent("Halo").enabled = true;
+				
+			ultimCanvi = Time.time;
+		}
+	}
+
+}
+
+function OnCollisionEnter(collision:Collision){
+	
+	//if(collision.gameObject.tag =="Player"){
+	//sistema provisional de deshabilitar el update, ja que no se si es vol que desaparegui l'halo o tota la bandera
+	if (deshabilitat == 0) {
+		audioPowerUp.Play();
+	    bandera.GetComponent("Halo").enabled = false;
+		deshabilitat = 1;
+	}
+}
