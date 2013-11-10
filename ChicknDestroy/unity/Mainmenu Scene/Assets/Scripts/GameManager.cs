3ce2488f1797322eb2b93379eed7460e4225686c
qfornaguera <qfornaguera@gmail.com>
2013-11-10 20:00:58 +0100

    #202 done and ready to be tested

--- ChicknDestroy/unity/Mainmenu Scene/Assets/Scripts/GameManager.cs
+++ ChicknDestroy/unity/Mainmenu Scene/Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour {
 	private static int SCORE_TO_WIN = 1000;
 	//public GameObject player;
 	private GameCamera cam;
-	
+	private float loseOrWinTime = 5F;//time later the lose or win of a player (5 seconds Timer)
 	private bool winConditionLastUpdate;
 	private bool looseConditionLastUpdate;
 	
@@ -31,6 +31,15 @@ public class GameManager : MonoBehaviour {
 		if (endRequested) {
 			endGame();
 		}
+		if(looseCondition() || winCondition())//when win or lose condition is true countdown the timer
+			loseOrWinTime -= Time.deltaTime;
+	}
+	
+	void OnGUI(){//Show the banner only when win condition or lose condition is true
+		if(looseCondition())
+			GUI.DrawTexture(new Rect(Screen.width/4,Screen.height/8,500,500),Resources.Load ("lose") as  Texture);
+		else if(winCondition())
+			GUI.DrawTexture(new Rect(Screen.width/4,Screen.height/8,500,500),Resources.Load ("win") as  Texture);
 	}
 	
 	private void fireWinNotification() {
@@ -57,6 +66,7 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	private void endGame() {
-		Application.LoadLevel(0);
+		if(loseOrWinTime <= 0F)//when 5 seconds of the timer elapsed return to main menu
+			Application.LoadLevel(0);
 	}
 }
