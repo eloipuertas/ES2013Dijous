5a4195a62433e99bf1c27b3ae3f9cb1cbe0bc6ed
armi7 <armillas7@hotmail.com>
2013-11-18 12:58:18 +0100

    #296 Afegit parpadeig al player en rebre dany
    De moment només parpadeja quant toca el foc. En l'intgeració s'afegiran
    la resta de casos. Per testejar

---
+++ ChicknDestroy/unity/Mainmenu Scene/Assets/Special_effects/Scripts/DanyPlayer.js
@@ -0,0 +1,49 @@
+﻿var cosPlayer:GameObject;
+var escopetaPlayer:GameObject;
+
+private var fetDany;
+private var tempsActual;
+private var lastTime;
+private var minTime;
+private var tempsEntrat;
+private var tempsParpadejant;
+
+function Start () {
+	fetDany = 0;
+	minTime = 0.15;
+	tempsParpadejant = 2;
+}
+
+function Update () {
+	if (fetDany == 1) {
+		if ((Time.time - lastTime) > minTime) {
+			if (cosPlayer.renderer.enabled) {
+				cosPlayer.renderer.enabled = false;
+				escopetaPlayer.renderer.enabled = false;
+			}
+			else {
+				cosPlayer.renderer.enabled = true;
+				escopetaPlayer.renderer.enabled = true;
+			}
+			lastTime = Time.time;
+		}
+		
+		if (Time.time - tempsEntrat > tempsParpadejant){
+			fetDany = 0;
+			cosPlayer.renderer.enabled = true;
+			escopetaPlayer.renderer.enabled = true;
+		}
+	}
+}
+
+function OnCollisionEnter(collision:Collision){
+	if(collision.gameObject.tag =="foc"){
+		if (fetDany == 0) {
+			fetDany = 1;
+			cosPlayer.renderer.enabled = false;
+			escopetaPlayer.renderer.enabled = false;
+			lastTime = Time.time;
+			tempsEntrat = Time.time;
+		}
+	}
+}
