1c5a440fb646279c3c689e9ad42b1e210f5a99c0
LynosSorien <lynossorien@gmail.com>
2013-11-29 00:06:14 +0100

    #401 Done
    TimerPool is done. Problem: every time a timer must be start, it must to
    reinitialize it (because for the "Elapsed" element inside the timers of
    the Unity) don't know how to solve without reinitializating the object.
    The object works fine. Ready for Test.

---
+++ ChicknDestroy/unity/Mainmenu Scene/Assets/Scripts/Time/TimerPool.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Timers;
+
+
+public class TimerPool
+{
+	private Timer[] timers;
+	public TimerPool (int amount)
+	{
+		this.timers = new Timer[amount];
+		for(int i = 0;i<amount;i++) this.timers[i] = new Timer();
+	}
+	public void start(long millis, Func<object,System.Timers.ElapsedEventArgs,Void> method) {
+		bool flag=false;
+		while (!flag) {
+			for (int i = 0;i<this.timers.Length;i++) {
+				if(this.timers[i].Enabled == false) {
+					this.timers[i] = new Timer();
+					this.timers[i].Elapsed += new ElapsedEventHandler(method);
+					this.timers[i].Interval = millis;
+					this.timers[i].Start();
+					this.timers[i].AutoReset = false;
+					flag = true;
+					break;
+				}
+			}
+		}
+	}
+}
+
